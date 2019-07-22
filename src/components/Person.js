c5135103f5dc3e34287420dc129b2dbfdc2da885
ibrahimmanaf <imanaf@algorisdigital.com>
2019-07-22 19:07:15 +0530

    router prompt added

--- src/components/Person.js
+++ src/components/Person.js
@@ -1,11 +1,27 @@
-import React from 'react'
+import React, { Component } from 'react'
+import { Prompt } from 'react-router-dom'
 
-function Person(props) {
-    return (
-        <div>
-            this is person page {props.match.params.personName}
-        </div>
-    )
+export default class Person extends Component {
+
+    state = {
+        text: "",
+        isFormStarted: false
+    }
+
+    onChangeText = (e) => {
+        this.setState({
+            text: e.target.value,
+            isFormStarted: true
+        })
+    }
+
+    render() {
+        return (
+            <div>
+                <Prompt when={this.state.isFormStarted} message="are u sure to leave?" />
+                this is person page {this.props.match.params.personName}
+                <input type="text" value={this.state.text} onChange={this.onChangeText} />
+            </div>
+        )
+    }
 }
-
-export default Person;
